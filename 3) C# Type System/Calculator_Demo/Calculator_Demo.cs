400577a626845164ab80871dc8c94a768f52f67f
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-03-31 20:01:45 +0530

    31 -03-23

---
+++ 3) C# Type System/Calculator_Demo/Calculator_Demo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Calculator_Demo
+{
+    class Program
+    {
+        public static void Addition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Addition Result is {result}");
+        }
+
+        public static void Substraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine($"Addition Result is {result}");
+        }
+
+        public static void Multiplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine($"Multiplication Result is {result}");
+        }
+
+        public static void Divison(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine($"Divison Result is {result}");
+        }
+        static void Main(string[] args)
+        {
+            //Asking user for two numbers
+            //First number
+            Console.WriteLine("Please enter first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            //second number
+            Console.WriteLine("Please enter first number:");
+            int num2 = int.Parse(Console.ReadLine());
+            //Operator selection
+            Console.WriteLine("Please enter operator (+, -, *, /):");
+            string op = Console.ReadLine();
+
+            if (op == "+")
+            {
+                Program.Addition(num1, num2);
+            }
+            else if (op == "-")
+            {
+                Program.Substraction(num1, num2);
+            }
+            else if (op == "*")
+            {
+                Program.Multiplication(num1, num2);
+            }
+            else if (op == "/")
+            {
+                Program.Divison(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+        }
+
+    }
+}
---
+++ 3) C# Type System/Ref_and _Out_Keywords/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ref_and__Out_Keywords
+{
+    class Program
+    {   
+        //PASS BY VALUE
+        public static void PassByValue(int a)
+        {
+            a = a + 10;
+            Console.WriteLine($"value of 'a' is {a}");
+        }
+        
+        //PASS BY REF
+        public static void PassByRef(ref int b)
+        {
+            b = b + 10;
+            Console.WriteLine($"value of 'b' is {b}");
+        }
+
+        static void Main(string[] args)
+        {
+            //For PASS BY VALUE
+            int value = 5;
+            PassByValue(value);
+            Console.WriteLine(value);
+
+            //For PASS BY REF
+            int valueee = 5;
+            PassByRef(ref valueee);
+            Console.WriteLine(valueee);
+        }
+    }
+}
