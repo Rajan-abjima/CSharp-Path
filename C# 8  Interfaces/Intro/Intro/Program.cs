236f1d841aa2dd232ed028e8b2ba4aa1bbc88113
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-06 19:31:31 +0530

    06-04-23

---
+++ C# 8  Interfaces/Intro/Intro/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Intro
+{   
+    interface IEmployees
+    {
+        void show();
+    }
+
+    class PartTimeEmployees : IEmployees
+    {
+        public void show() 
+        {
+            Console.WriteLine("This is method of IEmployee interface.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployees pte = new PartTimeEmployees();
+            pte.show();
+        }
+    }
+}
---
+++ Given Assignments/Intro/Intro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Intro
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string test = "Hello";
+                int i, j;
+                for (i = 0; i < test.Length; i++)
+                {
+                    for (j = i; j < test.Length; j++)
+                    {
+                        Console.Write(test.Substring(i, (j - i) + 1) + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
