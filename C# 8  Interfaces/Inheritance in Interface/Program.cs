71e5cde923be8c396445918a8a98d5db39b291a4
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-06 19:54:03 +0530

    06-04-23

---
+++ C# 8  Interfaces/Inheritance in Interface/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Inheritance_in_Interface
+{   
+    interface i1
+    {
+        void print();
+    }
+    interface i2
+    {
+        void print1();
+    }
+    interface i3 : i1, i2
+    {
+        void print2();
+    }
+
+
+    class Program : i3
+    {
+        public void print()
+        {
+            Console.WriteLine("This is a method of Interface 1");
+        }
+        public void print1()
+        {
+            Console.WriteLine("This is a method of Interface 2");
+        }
+        public void print2()
+        {
+            Console.WriteLine("This is a method of Interface 3");
+        }
+
+        static void Main(string[] args)
+        {
+            //Program p = new Program();
+            //p.print();
+            //p.print1();
+            //p.print2();
+
+            i1 myinterface = new Program();
+            myinterface.print();
+
+        }
+    }
+}
