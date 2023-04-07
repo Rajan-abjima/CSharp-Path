76994b7738e059efcb485c0e96aaef7bc0032e33
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-07 20:03:36 +0530

    07-04-23

---
+++ C# 8  Interfaces/Explicit_Implementation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Explicit_Implementation
+{
+    interface i1
+    {
+        void show();
+    }
+
+    interface i2
+    {
+        void show();
+    }
+
+    class MyClass : i1, i2
+    {
+        void i1.show()
+        {
+            Console.WriteLine("This is a method of interface i1  !!");
+        }
+
+        void i2.show()
+        {
+            Console.WriteLine("This is a method of interface i2  !!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Explicit Implementation =>");
+
+
+            i1 obj4 = new MyClass();
+            obj4.show();
+            i2 obj5 = new MyClass();
+            obj5.show();
+        }
+    }
+}
---
+++ C# 8  Interfaces/Multiple Inheritance with Interface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace Multiple_Inheritance_with_Interface
+{   
+    class Person
+    {
+        public void show()
+        {
+            Console.WriteLine("This is method of Person class");
+        }
+    }
+    interface Employee
+    {
+        void show();
+    }
+    interface Employee1
+    {
+        void show();
+    }
+
+    class Teacher : Person, Employee, Employee1
+    {
+        void Employee.show()
+        {
+            Console.WriteLine("This is a method of Employee Interface");
+        }
+        void Employee1.show()
+        {
+            Console.WriteLine("This is a method of Employee 1 Interface");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher t = new Teacher();
+            t.show();
+            Employee e = new Teacher();
+            e.show();
+            Employee1 e1 = new Teacher();
+            e1.show();
+            
+
+        }
+    }
+}
