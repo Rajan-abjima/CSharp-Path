33bb627c0a0b86296edf67f1aec266077e542c4e
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-08 19:17:05 +0530

    08-04-23

---
+++ C# 9 Generics/GENERICS_METHODS/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GENERICS_METHODS
+{
+    class Example
+    {
+        public static void showArray<T>(T[] arr) 
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[3];
+            Numbers[0] = 11;
+            Numbers[1] = 22;
+            Numbers[2] = 33;
+
+            
+            string[] Names= { "Aman", "Rahul", "Amir" };
+            Double[] Points = { 2.6, 4.3, 2, 4 };
+            
+            Example.showArray(Numbers);
+            Example.showArray(Names);
+            Example.showArray(Points);
+        }
+    }
+}
