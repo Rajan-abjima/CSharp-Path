4ac14c4ba7e61ecb0445bcd57380d86170b97cdf
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-11 19:17:25 +0530

    11-04-23

---
+++ C# 9 Generics/Wired Brain Coffee App/WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,12 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack<T>
+    {
+        private readonly T[] _items;
+        private int _CurrentIndex = -1;
+        public SimpleStack() => _items = new T[10];
+        public int count => _CurrentIndex + 1;
+        public void Push(T item) => _items[++_CurrentIndex] = item;
+        public T Pop() => _items[_CurrentIndex--];
+    }
+}
